1387f9d42509a7ed3b4fdb46a3ab04b8dd3eb57e
Nguyen Hoang Giang <hoanggiangd19bk@gmail.com>
2017-12-04 02:09:28 +0700

    Add new file

---
+++ xuly.js
@@ -0,0 +1,9 @@
+function doimau(){
+	document.getElementById('tieude').style.color = "red";
+	document.getElementById('tieude2').style.color = "red";
+	document.getElementById('tieude3').style.color = "red";
+	document.getElementById('tieude4').style.color = "red";
+	document.getElementById('tieude5').style.color = "red";
+	document.getElementById('tieude6').style.color = "red";
+	document.getElementById('tieude7').style.color = "red";
+}
